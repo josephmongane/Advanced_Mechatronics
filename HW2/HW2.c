4c3fb15dccb519c72dd545d6e77fc088aafc234d
josephmongane <joe.mongane@gmail.com>
2026-04-07 15:59:23 -0500

    Initial Homework 2

---
+++ HW2/HW2.c
@@ -0,0 +1,33 @@
+#include "HW2.h"
+
+int main()
+{
+    stdio_init_all();
+    while (true) {
+        int i; 
+        for (i=10; i<=170; i++) {
+            set_servo_postion(i);
+            sleep_ms(20);
+        }
+    }
+}
+
+void init_servo_pwm() {
+    // Initializing the Pulse with modulation of the servo motor
+    gpio_set_function(SERVO, GPIO_FUNC_PWM); // Set the LED Pin to be PWM
+    uint slice_num = pwm_gpio_to_slice_num(SERVO); // Get PWM slice number
+    // the clock frequency is 150MHz divided by a float from 1 to 255
+    float div = 50; // must be between 1-255
+    pwm_set_clkdiv(slice_num, div); // sets the clock speed
+    uint16_t wrap = 60000;
+    // set the PWM frequency and resolution
+    // this sets the PWM frequency to 150MHz / div / wrap
+    pwm_set_wrap(slice_num, wrap);
+    pwm_set_enabled(slice_num, true); // turn on the PWM
+}
+
+void set_servo_postion(int angle) {
+    int duty;
+    duty = angle*60000/180; 
+     pwm_set_gpio_level(SERVO, duty);
+}
---
+++ HW2/HW2.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+#define SERVO 15
+
+void init_servo_pwm(); 
+void set_servo_postion(int angle); 
