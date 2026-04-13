c2e5c84e59da4fc9d30cc6a7f446397be50bf4b8
josephmongane <joe.mongane@gmail.com>
2026-04-13 16:59:10 -0500

    Update HW3.c

--- HW3/HW3.c
+++ HW3/HW3.c
@@ -25,35 +25,39 @@ int main()
 
 
     while (true) {
-        read_reg[0] = 0x09;
-        uint8_t pin_bit = 0b1;
-        i2c_write_blocking(i2c_default, ADDR, &read_reg[0], 1, true);  // true to keep host control of bus
-        i2c_read_blocking(i2c_default, ADDR, &read_reg[1], 1, false);  // false - finished with bus
+        int result = read_mpc(0);
+        write_mpc(6, 1);
         sleep_ms(500); 
-        printf("Value %d\n", read_reg[1]);
+        write_mpc(6, 0);
+        printf("Value %d\n", result);
+        sleep_ms(500); 
     }
 }
 
 void init_mpc() {
     uint8_t buf[2]; 
     buf[0] = 0x0;
-    buf[1] = 0b01111111;
+    buf[1] = 0b00111111;
     i2c_write_blocking(i2c_default, ADDR, buf, 2, false); 
 }
 
 int read_mpc(int pin) {
+    int out; 
     uint8_t read_reg[2];
     read_reg[0] = 0x09;
     uint8_t pin_bit = 0b1 << pin;
     i2c_write_blocking(i2c_default, ADDR, &read_reg[0], 1, true);  // true to keep host control of bus
     i2c_read_blocking(i2c_default, ADDR, &read_reg[1], 1, false);  // false - finished with bus
-    return (pin_bit & read_reg[1]) >> pin;
+    out = (pin_bit & read_reg[1]) >> pin;
+    return out;
 }
 
 void write_mpc(int pin, int value) {
     uint8_t write_buf[2];
-    write_buf[1] = 0b1 << pin; 
     write_buf[0] = 0x0A; 
+    i2c_write_blocking(i2c_default, ADDR, &write_buf[0], 1, true);  // true to keep host control of bus
+    i2c_read_blocking(i2c_default, ADDR, &write_buf[1], 1, false);  // false - finished with bus
+    write_buf[1] = (write_buf[1]) | (value << pin); 
     i2c_write_blocking(i2c_default, ADDR, write_buf, 2, false); 
 }
 
