7068dace9fa235c79efe6c8bb1ba161adc674a9c
josephmongane <joe.mongane@gmail.com>
2026-04-28 12:40:24 -0500

    debug

--- spi_dac/spi_dac.c
+++ spi_dac/spi_dac.c
@@ -25,34 +25,43 @@ int main()
 
     spi_init(spi_default, 1000 * 1000); // the baud, or bits per second
 
+    gpio_init(PIN_CS); 
+    gpio_set_dir(PIN_CS, GPIO_OUT);
+
+
     gpio_set_function(PICO_DEFAULT_SPI_RX_PIN, GPIO_FUNC_SPI);
     gpio_set_function(PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI);
     gpio_set_function(PICO_DEFAULT_SPI_TX_PIN, GPIO_FUNC_SPI);
 
+    while(!stdio_usb_connected()); 
+
     while (true) {
         // calll the write dac function.
+        /*
         float t = 0;
         float freq = 2;
         t = t + 0.1;
         float voltage = (sin(2 * 3.14159 * freq * t) + 1) * 3.3 / 2.0; 
-        write_dac(1, voltage); 
-        sleep_ms(10);
+        */
+        write_dac(0, 3.0); 
+        sleep_ms(1000);
     }
 }
 
 void write_dac(int chan, float voltage) {
+    printf("voltage level set at %f\n", voltage);
     uint8_t data[2];
     data[0] = 0b01110000; 
     // Chosing the channel and the chip
-    data[0] = ( data[0] | ((chan & 0b1)<<7) ); 
+    data[0] = (data[0] | ((chan & 0b1)<<7)); 
 
     // converting a float to a bianary quant and 
     uint16_t bit_volt = voltage*1023 / 3.3;
     data[0] = ( data[0] | ((bit_volt>>6) & 0b0000001111) );
+    data[1] = (bit_volt << 2) & 0xFF; 
 
-    data[1] = (bit_volt << 2)& 0xFF; 
+    printf("data array = %d %d\n", data[0], data[1]);
 
-    
     cs_select(PIN_CS);
     spi_write_blocking(SPI_PORT, data, 2); // where data is a uint8_t array with length len
     cs_deselect(PIN_CS);
