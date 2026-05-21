dde3816720c0b34acc7e84317b6d5fbff6b9a43c
josephmongane <joe.mongane@gmail.com>
2026-05-21 00:52:32 -0500

    Uart Communication
    testing and debugging communication on the pico and the Stm 32

---
+++ pico_uart/pico_uart.c
@@ -0,0 +1,61 @@
+#include <pico/stdio_usb.h>
+#include <pico/time.h>
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/uart.h"
+
+// UART defines
+// By default the stdout UART is `uart0`, so we will use the second one
+#define UART_ID uart0
+#define BAUD_RATE 115200
+
+// Use pins 4 and 5 for UART1
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
+
+void uart_read_string(char *buf, size_t max_len) {
+    size_t i = 0;
+    while (i < max_len - 1) {
+        // Block until a character is available
+        char c = uart_getc(UART_ID);
+        // Treat newline or carriage return as end of string
+        if (c == '\n' || c == '\r') {
+            break;
+        }
+        buf[i++] = c;
+    }
+    buf[i] = '\0';  // Null-terminate the string
+}
+
+int main() {
+    // Initialize chosen serial port (USB CDC for printf)
+    stdio_init_all();
+
+    // Initialize UART hardware
+    uart_init(UART_ID, BAUD_RATE);
+
+    // Set the GPIO pin mux to the UART function
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+
+    char buffer[1000];
+
+    while(!stdio_usb_connected());
+
+    sleep_ms(10);
+
+    char send_string[1000]; 
+    sprintf(send_string, "Hello %d\r\n", 5);
+    uart_puts(uart0, send_string); 
+
+    while (true) {
+        uart_read_string(buffer, sizeof(buffer));
+        printf("Received: %s\n", buffer);
+        
+        // Only send again after we've finished reading
+        uart_puts(UART_ID, send_string);
+    }
+
+    return 0;
+}
