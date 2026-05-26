1f8971894767b44c3fca7eb620ceb877e0da5a0b
josephmongane <joe.mongane@gmail.com>
2026-05-26 13:26:14 -0500

    Initial changes

--- force_sense/force_sense.c
+++ force_sense/force_sense.c
@@ -2,12 +2,38 @@
 #include "pico/stdlib.h"
 
 
+#define SCK_PIN
+#define DT_PIN
+
+
+void init_hx711(); 
+
+
 int main()
 {
     stdio_init_all();
 
     while (true) {
-        printf("Hello, world!\n");
+        // Wait for user to send a n value
+
+        // Read N times 
+
+        // Filter data 
+
+        // Print to user orignial data, filtered data, and time in ms
+
+
         sleep_ms(1000);
     }
 }
+
+
+// Inittialization functions for the Force sensor 
+void init_hx711() {
+    GPIO_init(); 
+
+}
+
+// Read the 24 bits from the force sensor 
+
+//
